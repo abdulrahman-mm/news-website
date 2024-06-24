a1ebefb7bc497b2a0eb33605cf27769d626e3205
abdular01 <abdular025@gmail.com>
2024-06-24 21:08:44 +0530

    api call is changed to async await function

--- src/main.js
+++ src/main.js
@@ -7,12 +7,18 @@ function Main({ category, darkMode }) {
   const [valueFromApi, SetValueFromApi] = useState([]);
 
   useEffect(() => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=5067406ab57340b9a6184676af7c6160`;
+    async function apiCall() {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=5067406ab57340b9a6184676af7c6160`;
 
-    axios
-      .get(url)
-      .then((res) => SetValueFromApi(res.data.articles))
-      .catch((err) => console.log("error", err));
+      try {
+        let apiData = await axios.get(url);
+        SetValueFromApi(apiData.data.articles);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    apiCall();
   }, [category]);
 
   useEffect(() => {
@@ -24,87 +30,44 @@ function Main({ category, darkMode }) {
       className={`container mt-5 p-5 ${darkMode ? "dark-mode" : "light-mode"}`}
     >
       <div className="row">
-        {/* {valueFromApi
+        {valueFromApi
           .filter((item) => item.author !== null)
           .map((item, index) => (
             <div key={index} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
-              <a
-                href={item.url}
-                target="_blank"
-                rel="noreferrer"
-                style={{ textDecoration: "none" }}
+              <div
+                className={`card h-100 ${
+                  darkMode ? "dark-mode" : "light-mode"
+                }`}
               >
-                <div
-                  className={`card h-100 ${
-                    darkMode ? "dark-mode" : "light-mode"
-                  }`}
-                >
-                  <div className="image-container">
-                    <img
-                      className="card-img-top"
-                      src={item.urlToImage ? item.urlToImage : image}
-                      alt="News"
-                      style={{ objectFit: "initial", height: "150px" }}
-                    />
-                  </div>
-                  <div className="card-body d-flex flex-column">
-                    <h5 className="card-title">
-                      {item.title
-                        ? item.title.length > 200
-                          ? item.title.slice(0, 200) + "...."
-                          : item.title
-                        : ""}
-                    </h5>
-                    <a
-                      href={item.url}
-                      target="_blank"
-                      className="btn btn-secondary mt-auto"
-                      style={{ width: "150px" }}
-                    >
-                      Read More
-                    </a>
-                  </div>
+                <div className="image-container">
+                  <img
+                    className="card-img-top"
+                    src={item.urlToImage ? item.urlToImage : image}
+                    alt="News"
+                    style={{ objectFit: "initial", height: "150px" }}
+                  />
                 </div>
-              </a>
+                <div className="card-body d-flex flex-column">
+                  <h5 className="card-title">
+                    {item.title
+                      ? item.title.length > 200
+                        ? item.title.slice(0, 200) + "...."
+                        : item.title
+                      : ""}
+                  </h5>
+                  <a
+                    href={item.url}
+                    target="_blank"
+                    className="btn btn-secondary mt-auto"
+                    style={{ width: "150px" }}
+                    rel="noreferrer"
+                  >
+                    Read More
+                  </a>
+                </div>
+              </div>
             </div>
-          ))} */}
-          {valueFromApi
-  .filter((item) => item.author !== null)
-  .map((item, index) => (
-    <div key={index} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
-      <div
-        className={`card h-100 ${darkMode ? "dark-mode" : "light-mode"}`}
-      >
-        <div className="image-container">
-          <img
-            className="card-img-top"
-            src={item.urlToImage ? item.urlToImage : image}
-            alt="News"
-            style={{ objectFit: "initial", height: "150px" }}
-          />
-        </div>
-        <div className="card-body d-flex flex-column">
-          <h5 className="card-title">
-            {item.title
-              ? item.title.length > 200
-                ? item.title.slice(0, 200) + "...."
-                : item.title
-              : ""}
-          </h5>
-          <a
-            href={item.url}
-            target="_blank"
-            className="btn btn-secondary mt-auto"
-            style={{ width: "150px" }}
-            rel="noreferrer"
-          >
-            Read More
-          </a>
-        </div>
-      </div>
-    </div>
-  ))}
-
+          ))}
       </div>
     </div>
   );
